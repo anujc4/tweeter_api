be0354e9ec6c3e9b0756f35850211abe06af7451
Akshit Shrivastava <akshit19991024@gmail.com>
2021-01-26 20:36:32 +0530

    Update user.go

--- handler/user.go
+++ handler/user.go
@@ -3,12 +3,15 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
-
+	"strconv"
 	"github.com/anujc4/tweeter_api/internal/app"
 	"github.com/anujc4/tweeter_api/model"
 	"github.com/anujc4/tweeter_api/request"
 	"github.com/anujc4/tweeter_api/response"
 	"github.com/gorilla/schema"
+	"github.com/gorilla/mux"
+
+
 )
 
 // Set a Decoder instance as a package global, because it caches
@@ -61,15 +64,77 @@ func (env *HttpApp) GetUsers(w http.ResponseWriter, req *http.Request) {
 
 func (env *HttpApp) GetUserByID(w http.ResponseWriter, req *http.Request) {
 	// TODO: Implement this
-	app.RenderJSON(w, "Not yet implemented!")
+	params := mux.Vars(req)
+	id1:= params["user_id"]
+	id, err1 := strconv.Atoi(id1)
+    if err1 != nil {
+      app.RenderErrorJSON(w, app.NewParseFormError(err1))
+    }
+
+	if err := req.ParseForm(); err != nil {
+		app.RenderErrorJSON(w, app.NewParseFormError(err))
+		return
+	}
+	appModel := model.NewAppModel(req.Context(), env.DB)
+	users, err := appModel.GetUserByID(id)
+	if err != nil {
+		app.RenderErrorJSON(w, err)
+		return
+	}
+	resp := response.TransformUserResponse(*users)
+	app.RenderJSON(w, resp)
+
+	// app.RenderJSON(w, "Not yet implemented!")
 }
 
 func (env *HttpApp) UpdateUser(w http.ResponseWriter, req *http.Request) {
 	// TODO: Implement this
-	app.RenderJSON(w, "Not yet implemented!")
+	var request request.CreateUserRequest
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&request); err != nil {
+		app.RenderErrorJSON(w, app.NewError(err))
+		return
+	}
+
+	params := mux.Vars(req)
+	id1:= params["user_id"]
+	id, err1 := strconv.Atoi(id1)
+		if err1 != nil {
+				app.RenderErrorJSON(w, app.NewParseFormError(err1))
+		}
+		if err := request.ValidateCreateUserRequest(); err != nil {
+	 		app.RenderErrorJSON(w, app.NewError(err))
+	 		return
+	 	}
+
+	appModel := model.NewAppModel(req.Context(), env.DB)
+	err := appModel.UpdateUser(&request,id)
+	if err != nil {
+		app.RenderErrorJSON(w, err)
+		return
+	}
+	app.RenderJSON(w, "updated")
+	// app.RenderJSON(w, "Not yet implemented!")
 }
 
 func (env *HttpApp) DeleteUser(w http.ResponseWriter, req *http.Request) {
 	// TODO: Implement this
-	app.RenderJSON(w, "Not yet implemented!")
+	params := mux.Vars(req)
+	id1:= params["user_id"]
+	id, err1 := strconv.Atoi(id1)
+		if err1 != nil {
+			app.RenderErrorJSON(w, app.NewParseFormError(err1))
+		}
+	if err := req.ParseForm(); err != nil {
+		app.RenderErrorJSON(w, app.NewParseFormError(err))
+		return
+	}
+	appModel := model.NewAppModel(req.Context(), env.DB)
+	err := appModel.DeleteUser(id)
+	if err != nil {
+		app.RenderErrorJSON(w, err)
+		return
+	}
+	app.RenderJSON(w, "deleted")
+	// app.RenderJSON(w, "Not yet implemented!")
 }
