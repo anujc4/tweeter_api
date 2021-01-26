ae93c3961715a432be2d39adc8180ed0ae8df3ce
Piyush Jha <piyushjha8164@gmail.com>
2021-01-26 22:02:18 +0530

    Updated request\user.go, added new structs and validations
    - Added request structs for `UpdateUserRequest` and `GetUserByIDRequest`
    
    - Added validations for those two requests too.
    
    - `UpdateUserRequest`  requires a password to delete or update a user.

--- request/user.go
+++ request/user.go
@@ -26,7 +26,6 @@ type GetUsersRequest struct {
 	FirstName string `schema:"first_name"`
 	Email     string `schema:"email"`
 }
-
 // eg. Validation if either phone no or email was required
 // func (r GetUsersRequest) ValidateGetUsersRequest() error {
 // 	return validation.ValidateStruct(&r,
@@ -34,3 +33,31 @@ type GetUsersRequest struct {
 // 		validation.Field(&r.FirstName, validation.Required.When(r.Email == "").Error("Either phone or Email is required.")),
 // 	)
 // }
+
+
+type GetUserByIDRequest struct{
+	ID uint `schema:"id"`
+}
+
+func (r GetUserByIDRequest) ValidateGetUserByIDRequest() error {
+	return validation.ValidateStruct(&r,
+		validation.Field(&r.Email, validation.Required, validation.Match(regexp.MustCompile("[0-9]"))),
+}
+
+type UpdateUserRequest struct {
+	ID uint `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+//Password & User_ID is required to update/delete a user
+func (r UpdateUserRequest) UpdateUserRequestIDRequest() error {
+	return validation.ValidateStruct(&r,
+		validation.Field(&r.Email, validation.Required, validation.Match(regexp.MustCompile("[0-9]"))),
+		validation.Field(&r.FirstName, validation.Length(3, 20)),
+		validation.Field(&r.LastName, validation.Length(3, 20)),
+		validation.Field(&r.Password, validation.Required, validation.Length(5, 50)),
+}
+
