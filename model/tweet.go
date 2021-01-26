edc82463d50ef4b4041e129a9eab223d6ffce68f
heeman <pandeyhimanshu709@gmail.com>
2021-01-26 10:01:35 +0530

    Added tweet model in models/tweet.go

--- model/tweet.go
+++ model/tweet.go
@@ -1,1 +1,14 @@
 package model
+
+import (
+	"gorm.io/gorm"
+)
+
+// Tweet represents tweet object in our app
+type Tweet struct {
+	gorm.Model  //gorm.Model includes fields like ID, CreatedAt and UpdatedAt
+	Content     string
+	ParentTweet uint
+	UserID      uint
+	User        User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
