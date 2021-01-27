9b2f941495ca7d423498c37d4cbe1cfd67a1f9f7
siddhantlohia11 <sidhantlohia11@gmail.com>
2021-01-27 10:23:42 +0530

    Few Changes

--- request/tweet.go
+++ request/tweet.go
@@ -5,9 +5,3 @@ type CreateTweetRequest struct {
 	Content      string `json:"content,omitempty"`
 	Parent_tweet int    `json:"parent_tweet,omitempty"`
 }
-
-type GetTweetsRequest struct {
-	PaginationRequest
-	Content string `schema:"content"`
-	UserId  string `schema:"user_id"`
-}
